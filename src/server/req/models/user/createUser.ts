88ff90dbde835bd7a4d66c38f600e2863d0f1c59
chgibb <chris.gibb@outlook.com>
2016-11-10 09:04:02 -0500

    Removed refactored code

--- src/server/req/models/user/createUser.ts
+++ src/server/req/models/user/createUser.ts
@@ -12,30 +12,5 @@ export default function createUser(req : any,res : any,user : User,db : any) : b
         db,
         res
     );
-    /*let queries : Array<Array<string>> = new Array<Array<string>>();
-    queries = jsonFile.readFileSync("sql/queries/postUser.sql.json");
-    db.serialize
-    (
-        ()=>
-        {
-            for(let i : number = 0; i != queries.length; ++i)
-            {
-                db.get
-                (
-                    queries[i].join("\n"),
-                    [user.User_ID,user.First_Name,user.Last_Name,user.Created_At,user.Email],
-                    (err : string, row : Array<any>)=>
-                    {
-                        //res.send(row);
-                        if(err)
-                        {
-                            console.log(err);
-                            return;
-                        }
-                    }
-                )
-            };
-        }
-    );*/
     return true;
 }
--- src/server/req/models/workout/createWorkout.ts
+++ src/server/req/models/workout/createWorkout.ts
@@ -11,30 +11,5 @@ export default function createWorkout(workout : Workout,db : any) : boolean
         [workout.Workout_ID,workout.User_ID,workout.Workout_Name,workout.Date_Performed],
         db
     );
-    /*let queries : Array<Array<string>> = new Array<Array<string>>();
-    queries = jsonFile.readFileSync("sql/queries/postWorkout.sql.json");
-    db.serialize
-    (
-        ()=>
-        {
-            for(let i : number = 0; i != queries.length; ++i)
-            {
-                db.get
-                (
-                    queries[i].join("\n"),
-                    [workout.Workout_ID,workout.User_ID,workout.Workout_Name,workout.Date_Performed],
-                    (err : string, row : Array<any>)=>
-                    {
-                        //res.send(row);
-                        if(err)
-                        {
-                            console.log(err);
-                            return;
-                        }
-                    }
-                )
-            };
-        }
-    );*/
     return true;
 }
